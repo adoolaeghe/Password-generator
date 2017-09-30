fb0301c3e5be2d78de7e71bc970755140e6aaae1
Antoine Doolaeghe <antoinedoolaeghe@gmail.com>
2017-09-30 23:31:52 +0200

    added other tests for initialisation

--- lib/password_generator.rb
+++ lib/password_generator.rb
@@ -5,12 +5,13 @@ NUMBERS = "123456789"
 
 class Password_generator
 
-  attr_reader :length, :uppercase, :lowercase, :special
+  attr_reader :length, :uppercase, :lowercase, :number, :special
 
   def initialize(length, uppercase, lowercase, number, special)
     @length = length
     @uppercase = uppercase
     @lowercase = lowercase
     @special = special
+    @number = number
   end
 end
--- spec/unit/password_generator_spec.rb
+++ spec/unit/password_generator_spec.rb
@@ -8,5 +8,17 @@ describe Password_generator do
     it 'stores the given length' do
       expect(password_generator.length).to eq(length)
     end
+    it 'stores returns true for number' do
+      expect(password_generator.number).to be(true)
+    end
+    it 'stores returns true for special' do
+      expect(password_generator.special).to be(true)
+    end
+    it 'stores returns true for uppercase' do
+      expect(password_generator.uppercase).to be(true)
+    end
+    it 'stores returns true for lowercase' do
+      expect(password_generator.lowercase).to be(true)
+    end
   end
 end
